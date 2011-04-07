2143c8b78f246713ba2d4ae7c6cb9b2eeb1154c4
Rodrigo A. B. de Almeida <>
2011-04-07 09:48:56 +0000

    Added Flush() to BufferedImage and other minor changes in order to make the rendering tests work on OSX

--- src/test/java/agile2d/AgileSample.java
+++ src/test/java/agile2d/AgileSample.java
@@ -39,134 +39,133 @@ import java.nio.ByteBuffer;
  * @version $Revision$
  */
 public class AgileSample implements GLEventListener, KeyListener {
-    private AgileGraphics2D jgraphics;
-    private Component root;
-    private boolean interactive_antialias = false;
-    private static int NB_OF_SAMPLES_FOR_MULTISAMPLE = 4;
-    private boolean aglObjectCreated = false;
-    private AglTestContext context;
-    private int width, height;
-    BufferedImage buf_img;
+	private AgileGraphics2D jgraphics;
+	private Component root;
+	private boolean interactive_antialias = false;
+	private static int NB_OF_SAMPLES_FOR_MULTISAMPLE = 4;
+	private boolean aglObjectCreated = false;
+	private AglTestContext context;
+	private int width, height;
+	BufferedImage buf_img;
 
-    public void keyTyped(KeyEvent e){}
+	public void keyTyped(KeyEvent e){}
 
-    public void keyPressed(KeyEvent e){}
+	public void keyPressed(KeyEvent e){}
 
-    public void keyReleased(KeyEvent e){}
+	public void keyReleased(KeyEvent e){}
 
-    public AgileSample(Component root) {this.root = root;}
+	public AgileSample(Component root) {this.root = root;}
 
-    public AgileGraphics2D getAglInstance(){
-	if (aglObjectCreated==true)
-		return this.jgraphics;
-	else{
-		System.err.println("Error.\nAgileGraphics2D Object has not yet been created.");
-		return null;
-	}	
-    }
-
-    public void init(GLAutoDrawable drawable) {
-	GLU glu = new GLU();
-        jgraphics = new AgileGraphics2D(drawable);
-	aglObjectCreated=true;
-        GL2 gl = drawable.getGL().getGL2();
-        System.out.println("INIT GL IS: " + gl.getClass().getName());
-        System.out.println("GLU version is: " + glu.gluGetString(GLU.GLU_VERSION));
-
-	//Check if MULTISAMPLE is avaiable	
-	int[] buf = new int[2];
-	int[] samples = new int[2];
-	gl.glGetIntegerv(GL2.GL_SAMPLE_BUFFERS, buf, 0);
-	gl.glGetIntegerv(GL2.GL_SAMPLES, samples, 0);
-        System.out.println("Number of sample buffers: " + buf[0]);
-        System.out.println("Number of samples: " + samples[0]);
-
-	//Defines frequency in which buffers (back and front) are changed
-	//gl.setSwapInterval(1);
-  }
-
-    public void reshape(GLAutoDrawable arg0, int x, int y, int width, int height) {
-        if (root != null) {
-            root.setSize(width, height);
-        }
-	this.width=width;
-	this.height=height;
-    }
-
-    public void display(GLAutoDrawable drawable) {
-	GL2 gl = drawable.getGL().getGL2();
-        AgileState glState = AgileState.get(gl);
-		
-        // Call the glClear to clear the background
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-
-        // Restore all the Java2D Graphics defaults
-        jgraphics.resetAll(drawable);
-	jgraphics.setStroke(new BasicStroke(2));
-
-	//Find out what is the standard Graphics2d Font setting
-	Component c = (Component)drawable;
-	Graphics2D g2d_sample = (Graphics2D)c.createImage(1, 1).getGraphics();
-	jgraphics.setFont(g2d_sample.getFont());
-
-	if(interactive_antialias==true)
-		jgraphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-
-	//Paint sample primitives
-	jgraphics.setBackground(Color.WHITE);
-	jgraphics.clearRect(0, 0, width, height);
-	
-	//
-	//call test methods if context has been created		
-	if(context!=null)
-	      	context.drawStrategy(jgraphics);
-	else
-		System.out.println("AglTestContext has NOT been created");
-
-	buf_img = createImageFromBuffer(gl);	
-	
-    }
-
-    public BufferedImage getBufferedImage(){
-	return buf_img;	
-   } 
-
-    private BufferedImage createImageFromBuffer(GL2 gl){
-	ByteBuffer buffer = ByteBuffer.allocateDirect(width*height*4);
-	gl.glReadPixels(0, 0, width, height, GL.GL_RGBA, GL.GL_UNSIGNED_BYTE, buffer);
-	BufferedImage bImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-	int[] pixelInts = new int[ width*height ];
-
-// Points to first byte (red) in each row.
-	int p = width * height * 4; 
-	int q; // Index into ByteBuffer
-	int i = 0; // Index into target int[]
-	int w4 = width * 4; // Number of bytes in each row
-	for(int row=0; row<width; row++){
-		p -= w4;
-		q = p;
-		for(int col=0; col<width; col++){
-			int iR = buffer.get(q++);
-			int iG = buffer.get(q++);
-			int iB = buffer.get(q++);
-			int iA = buffer.get(q++);
-			pixelInts[i++] = 0xFF000000 | ((iR & 0x000000FF) << 16) | ((iG & 0x000000FF) << 8) | (iB & 0x000000FF);
-		}			
+	public AgileGraphics2D getAglInstance(){
+		if (aglObjectCreated==true)
+			return this.jgraphics;
+		else{
+			System.err.println("Error.\nAgileGraphics2D Object has not yet been created.");
+			return null;
+		}	
 	}
-	bImage.setRGB( 0, 0, width, height, pixelInts, 0, width);
-	return bImage;
-    }
 
-	
+	public void init(GLAutoDrawable drawable) {
+		GLU glu = new GLU();
+		jgraphics = new AgileGraphics2D(drawable);
+		aglObjectCreated=true;
+		GL2 gl = drawable.getGL().getGL2();
+		System.out.println("INIT GL IS: " + gl.getClass().getName());
+		System.out.println("GLU version is: " + glu.gluGetString(GLU.GLU_VERSION));
 
-    public void displayChanged(GLAutoDrawable drawable,boolean modeChanged,boolean deviceChanged) {}
+		//Check if MULTISAMPLE is avaiable	
+		int[] buf = new int[2];
+		int[] samples = new int[2];
+		gl.glGetIntegerv(GL2.GL_SAMPLE_BUFFERS, buf, 0);
+		gl.glGetIntegerv(GL2.GL_SAMPLES, samples, 0);
+		System.out.println("Number of sample buffers: " + buf[0]);
+		System.out.println("Number of samples: " + samples[0]);
 
-    public void dispose(GLAutoDrawable drawable) {}
+		//Defines frequency in which buffers (back and front) are changed
+		//gl.setSwapInterval(1);
+	}
 
-    public void setRoot(Component root) { this.root = root; }
+	public void reshape(GLAutoDrawable arg0, int x, int y, int width, int height) {
+		if (root != null) {
+			root.setSize(width, height);
+		}
+		this.width=width;
+		this.height=height;
+	}
 
-    public Component getRoot() { return root; }
+	public void display(GLAutoDrawable drawable) {
+		GL2 gl = drawable.getGL().getGL2();
+		AgileState glState = AgileState.get(gl);
 
-    public void setContext(AglTestContext context){ this.context = context; }
+		// Call the glClear to clear the background
+		gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+
+		// Restore all the Java2D Graphics defaults
+		jgraphics.resetAll(drawable);
+		jgraphics.setStroke(new BasicStroke(2));
+
+		//Find out what is the standard Graphics2d Font setting
+		Component c = (Component)drawable;
+		Graphics2D g2d_sample = (Graphics2D)c.createImage(1, 1).getGraphics();
+		jgraphics.setFont(g2d_sample.getFont());
+
+		if(interactive_antialias==true)
+			jgraphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+
+		//Paint sample primitives
+		jgraphics.setBackground(Color.WHITE);
+		jgraphics.clearRect(0, 0, width, height);
+
+		//
+		//call test methods if context has been created		
+		if(context!=null)
+			context.drawStrategy(jgraphics);
+		else
+			System.out.println("AglTestContext has NOT been created");
+
+		buf_img = createImageFromBuffer(gl);	
+
+	}
+
+	public BufferedImage getBufferedImage(){
+		return buf_img;	
+	} 
+
+	private BufferedImage createImageFromBuffer(GL2 gl){
+		ByteBuffer buffer = ByteBuffer.allocateDirect(width*height*4);
+		gl.glReadPixels(0, 0, width, height, GL.GL_RGBA, GL.GL_UNSIGNED_BYTE, buffer);
+		BufferedImage bImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		int[] pixelInts = new int[ width*height ];
+
+		//Points to first byte (red) in each row.
+		int p = width * height * 4; 
+		int q; // Index into ByteBuffer
+		int i = 0; // Index into target int[]
+		int w4 = width * 4; // Number of bytes in each row
+		for(int row=0; row<width; row++){
+			p -= w4;
+			q = p;
+			for(int col=0; col<width; col++){
+				int iR = buffer.get(q++);
+				int iG = buffer.get(q++);
+				int iB = buffer.get(q++);
+				int iA = buffer.get(q++);
+				pixelInts[i++] = 0xFF000000 | ((iR & 0x000000FF) << 16) | ((iG & 0x000000FF) << 8) | (iB & 0x000000FF);
+			}			
+		}
+		bImage.setRGB( 0, 0, width, height, pixelInts, 0, width);
+		return bImage;
+	}
+
+
+	public void displayChanged(GLAutoDrawable drawable,boolean modeChanged,boolean deviceChanged) {}
+
+	public void dispose(GLAutoDrawable drawable) {}
+
+	public void setRoot(Component root) { this.root = root; }
+
+	public Component getRoot() { return root; }
+
+	public void setContext(AglTestContext context){ this.context = context; }
 
 }
--- src/test/java/agile2d/test/TestAgileSample.java
+++ src/test/java/agile2d/test/TestAgileSample.java
@@ -15,7 +15,7 @@ import agile2d.AgileSample;
  * @author Rodrigo de Almeida
  * @version $Revision$
  *//**
-**/
+ **/
 
 import java.awt.Graphics2D;
 import java.awt.Frame;
@@ -51,301 +51,304 @@ import org.apache.batik.ext.awt.image.GraphicsUtil;
 
 
 public class TestAgileSample {
-    static GLProfile glp;
-    static Frame frame;
-    static Frame frameg2d;
-    static GLCanvas glCanvas;
-    static G2DSample g2dCanvas;
-    static AgileSample sample; 
-    static int width, height, win_x, win_y;
-    BufferedImage imgG2d, imgAg2d;
-    final int CAP_OFFSET = 15;
+	static GLProfile glp;
+	static Frame frame;
+	static Frame frameg2d;
+	static GLCanvas glCanvas;
+	static G2DSample g2dCanvas;
+	static AgileSample sample; 
+	static int width, height, win_x, win_y;
+	BufferedImage imgG2d, imgAg2d, image_dif;
+	final int CAP_OFFSET = 15;
 
-    @BeforeClass
-    public static void initClass() {
-        width = 512;
-        height = 512;
+	@BeforeClass
+	public static void initClass() {
+		width = 512;
+		height = 512;
 		win_x=80;
 		win_y=120;
-        glp = GLProfile.getDefault();
-        Assert.assertNotNull(glp);
-        GLCapabilities caps = new GLCapabilities(GLProfile.getDefault());
+		glp = GLProfile.getDefault();
+		Assert.assertNotNull(glp);
+		GLCapabilities caps = new GLCapabilities(GLProfile.getDefault());
 
-	frame = new Frame("AgileCanvas Test");
-        Assert.assertNotNull(frame);
+		frame = new Frame("AgileCanvas Test");
+		Assert.assertNotNull(frame);
 
-	glCanvas = new GLCanvas(caps);
-        Assert.assertNotNull(glCanvas);
-        frame.add(glCanvas);
-        frame.setLocation(win_x, win_y);
-        frame.setSize(width, height);
-
-
-	sample = new AgileSample(null);
-        glCanvas.addGLEventListener(sample);
-
-        frame.setUndecorated(true);
-        frame.setVisible(true);
-
-	glCanvas.repaint();
-
-        Assert.assertNotNull(frame);
-        Assert.assertNotNull(glCanvas);
+		glCanvas = new GLCanvas(caps);
+		Assert.assertNotNull(glCanvas);
+		frame.add(glCanvas);
+		frame.setLocation(win_x, win_y);
+		frame.setSize(width, height);
 
 
-	//Normal Frame
-	frameg2d = new Frame("G2DCanvas Test");
-	g2dCanvas = new G2DSample();
-        frameg2d.add(g2dCanvas);
-        frameg2d.setLocation(win_x+width, win_y);
-        frameg2d.setSize(width, height);
-        frameg2d.setUndecorated(true);
-        frameg2d.setVisible(true);
+		sample = new AgileSample(null);
+		glCanvas.addGLEventListener(sample);
 
-	g2dCanvas.repaint();
-}
+		frame.setUndecorated(true);
+		frame.setVisible(true);
 
-    @AfterClass
-    public static void releaseClass() {
-    }
+		glCanvas.repaint();
+
+		Assert.assertNotNull(frame);
+		Assert.assertNotNull(glCanvas);
 
 
-// TESTS SEQUENCE
+		//Normal Frame
+		frameg2d = new Frame("G2DCanvas Test");
+		g2dCanvas = new G2DSample();
+		frameg2d.add(g2dCanvas);
+		frameg2d.setLocation(win_x+width, win_y);
+		frameg2d.setSize(width, height);
+		frameg2d.setUndecorated(true);
+		frameg2d.setVisible(true);
 
-    @Test
-    public void prepareCanvas() throws InterruptedException {
-     	AglTestContext context = new AglTestContext(new AglTestStrategyClearRect());
-	endUnit(context, true, 100);
-    }
-
-    @Test
-    public void testDrawRect() throws InterruptedException {
-//	writePreviousDiff("cRect");
-        AglTestContext context = new AglTestContext(new AglTestStrategyDrawRect());
-	endUnit(context, true, 150);
-    }
-
-    @Test
-    public void testDrawRoundRect() throws InterruptedException {
-	writePreviousDiff("rect");
-        AglTestContext context = new AglTestContext(new AglTestStrategyDrawRoundRect());
-	endUnit(context, true, 150);
-    }
-
-    @Test
-    public void testDrawLine() throws InterruptedException {
-	writePreviousDiff("roundRect");
-        AglTestContext context = new AglTestContext(new AglTestStrategyDrawLine());
-	endUnit(context, true, 150);
-    }
-
-   @Test
-    public void testDrawOval() throws InterruptedException {
-	writePreviousDiff("line");
-        AglTestContext context = new AglTestContext(new AglTestStrategyDrawOval());
-	endUnit(context, true, 450);
-    }
-
-
-   @Test
-    public void testDrawString() throws InterruptedException {
-	writePreviousDiff("oval");
-        AglTestContext context = new AglTestContext(new AglTestStrategyDrawString());
-	endUnit(context, true, 1500);
-    }
-
-   @Test
-    public void testFillOval() throws InterruptedException {
-	writePreviousDiff("String");
-        AglTestContext context = new AglTestContext(new AglTestStrategyFillOval());
-	endUnit(context, true, 150);
-    }
-
-   @Test
-    public void testDrawAlpha() throws InterruptedException {
-	writePreviousDiff("FillOval");
-        AglTestContext context = new AglTestContext(new AglTestStrategyDrawAlpha());
-	endUnit(context, true, 3500);
-   }
-
-   @Test
-    public void testGradient() throws InterruptedException {
-	writePreviousDiff("drawAlpha");
-        AglTestContext context = new AglTestContext(new AglTestStrategyGradient());
-	endUnit(context, true, 800);
-   }
-
-   @Test
-    public void testStrokes() throws InterruptedException {
-	writePreviousDiff("drawGradient");
-        AglTestContext context = new AglTestContext(new AglTestStrategyStrokes());
-	endUnit(context, true, 1500);
-   }
-
-   @Test
-    public void testSetGetColor() throws InterruptedException {
-	writePreviousDiff("testStrokes");
-        AglTestContext context = new AglTestContext(new AglTestStrategySetGetColor());
-	endUnit(context, false, 30);
-	Color[] color_tmp = (Color[])context.getObjectsStrategy();
-        Assert.assertEquals(color_tmp[0].getRGB(), color_tmp[1].getRGB());
-    }
-
-
-   @Test
-    public void testSetGetBackground() throws InterruptedException {
-        AglTestContext context = new AglTestContext(new AglTestStrategySetGetBackground());
-	endUnit(context, false, 30);
-	Color[] color_tmp = (Color[])context.getObjectsStrategy();
-        Assert.assertEquals(color_tmp[0].getRGB(), color_tmp[1].getRGB());
-    }
-
-   @Test
-    public void testSetGetClipRect() throws InterruptedException {
-        AglTestContext context = new AglTestContext(new AglTestStrategySetGetClipRect());
-	endUnit(context, false, 30);
-	Rectangle[] clip_tmp = (Rectangle[])context.getObjectsStrategy();
-        Assert.assertEquals(clip_tmp[0], clip_tmp[1]);
-    }
-
-   @Test
-    public void testSetGetFont() throws InterruptedException {
-        AglTestContext context = new AglTestContext(new AglTestStrategySetGetFont());
-	endUnit(context, false, 80);
-	Font[] font_tmp = (Font[])context.getObjectsStrategy();
-        Assert.assertEquals(font_tmp[0], font_tmp[1]);
-    }
-
-   @Test
-    public void testGetFontMetrics() throws InterruptedException {
-        AglTestContext context = new AglTestContext(new AglTestStrategyGetFontMetrics());
-	endUnit(context, false, 120);
-	FontMetrics[] fontM_tmp = (FontMetrics[])context.getObjectsStrategy();
-        Assert.assertNotNull(fontM_tmp[0]);
-    }
-
-   @Test
-    public void testSetGetTransform() throws InterruptedException {
-        AglTestContext context = new AglTestContext(new AglTestStrategySetGetTransform());
-	endUnit(context, false, 30);
-	AffineTransform[] transf_tmp = (AffineTransform[])context.getObjectsStrategy();
-        Assert.assertEquals(transf_tmp[0], transf_tmp[1]);
-    }
-
-   @Test
-    public void testSetGetStroke() throws InterruptedException {
-        AglTestContext context = new AglTestContext(new AglTestStrategySetGetStroke());
-	endUnit(context, false, 10);
-	BasicStroke[] strk_tmp = (BasicStroke[])context.getObjectsStrategy();
-        Assert.assertEquals(strk_tmp[0], strk_tmp[1]);
-    }
-
-   //End test : It must be ***THE LAST ONE*** to be called in the list of tests
-   @Test
-    public void testMustBeLastOne() throws InterruptedException {
-	
-        frame.setVisible(false);
-        Assert.assertEquals(false, frame.isVisible());
-        frame.remove(glCanvas);
-        frame.dispose();
-        frame=null;
-        glCanvas=null;
-	}
-
-    public void endUnit(AglTestContext _context, boolean updateBothContexts, int delay) throws InterruptedException {
-	sample.setContext(_context);
-	glCanvas.repaint();
-
-	
-	//get Agile Ilage thanks to glReadPixels
-	imgAg2d = sample.getBufferedImage();
-
-	if(updateBothContexts==true){
-		g2dCanvas.setContext(_context);	
 		g2dCanvas.repaint();
+	}
+
+	@AfterClass
+	public static void releaseClass() {
+	}
+
+
+	// TESTS SEQUENCE
+
+	@Test
+	public void prepareCanvas() throws InterruptedException {
+		AglTestContext context = new AglTestContext(new AglTestStrategyClearRect());
+		endUnit(context, true, 200);
+	}
+
+	@Test
+	public void testDrawRect() throws InterruptedException {
+		//	writePreviousDiff("cRect");
+		AglTestContext context = new AglTestContext(new AglTestStrategyDrawRect());
+		endUnit(context, true, 200);
+	}
+
+	@Test
+	public void testDrawRoundRect() throws InterruptedException {
+		writePreviousDiff("rect");
+		AglTestContext context = new AglTestContext(new AglTestStrategyDrawRoundRect());
+		endUnit(context, true, 200);
+	}
+
+	@Test
+	public void testDrawLine() throws InterruptedException {
+		writePreviousDiff("roundRect");
+		AglTestContext context = new AglTestContext(new AglTestStrategyDrawLine());
+		endUnit(context, true, 200);
+	}
+
+	@Test
+	public void testDrawOval() throws InterruptedException {
+		writePreviousDiff("line");
+		AglTestContext context = new AglTestContext(new AglTestStrategyDrawOval());
+		endUnit(context, true, 200);
+	}
+
+	@Test
+	public void testDrawString() throws InterruptedException {
+		writePreviousDiff("oval");
+		AglTestContext context = new AglTestContext(new AglTestStrategyDrawString());
+		endUnit(context, true, 1500);
+	}
+
+	@Test
+	public void testFillOval() throws InterruptedException {
+		writePreviousDiff("String");
+		AglTestContext context = new AglTestContext(new AglTestStrategyFillOval());
+		endUnit(context, true, 150);
+	}
+
+	@Test
+	public void testDrawAlpha() throws InterruptedException {
+		writePreviousDiff("FillOval");
+		AglTestContext context = new AglTestContext(new AglTestStrategyDrawAlpha());
+		endUnit(context, true, 3500);
+	}
+
+	@Test
+	public void testGradient() throws InterruptedException {
+		writePreviousDiff("drawAlpha");
+		AglTestContext context = new AglTestContext(new AglTestStrategyGradient());
+		endUnit(context, true, 800);
+	}
+
+	@Test
+	public void testStrokes() throws InterruptedException {
+		writePreviousDiff("drawGradient");
+		AglTestContext context = new AglTestContext(new AglTestStrategyStrokes());
+		endUnit(context, true, 1500);
+	}
+
+	@Test
+	public void testSetGetColor() throws InterruptedException {
+		writePreviousDiff("testStrokes");
+		AglTestContext context = new AglTestContext(new AglTestStrategySetGetColor());
+		endUnit(context, false, 100);
+		Color[] color_tmp = (Color[])context.getObjectsStrategy();
+		Assert.assertEquals(color_tmp[0].getRGB(), color_tmp[1].getRGB());
+	}
+
+
+	@Test
+	public void testSetGetBackground() throws InterruptedException {
+		AglTestContext context = new AglTestContext(new AglTestStrategySetGetBackground());
+		endUnit(context, false, 100);
+		Color[] color_tmp = (Color[])context.getObjectsStrategy();
+		Assert.assertEquals(color_tmp[0].getRGB(), color_tmp[1].getRGB());
+	}
+
+	@Test
+	public void testSetGetClipRect() throws InterruptedException {
+		AglTestContext context = new AglTestContext(new AglTestStrategySetGetClipRect());
+		endUnit(context, false, 100);
+		Rectangle[] clip_tmp = (Rectangle[])context.getObjectsStrategy();
+		Assert.assertEquals(clip_tmp[0], clip_tmp[1]);
+	}
+
+	@Test
+	public void testSetGetFont() throws InterruptedException {
+		AglTestContext context = new AglTestContext(new AglTestStrategySetGetFont());
+		endUnit(context, false, 100);
+		Font[] font_tmp = (Font[])context.getObjectsStrategy();
+		Assert.assertEquals(font_tmp[0], font_tmp[1]);
+	}
+
+	@Test
+	public void testGetFontMetrics() throws InterruptedException {
+		AglTestContext context = new AglTestContext(new AglTestStrategyGetFontMetrics());
+		endUnit(context, false, 120);
+		FontMetrics[] fontM_tmp = (FontMetrics[])context.getObjectsStrategy();
+		Assert.assertNotNull(fontM_tmp[0]);
+	}
+
+	@Test
+	public void testSetGetTransform() throws InterruptedException {
+		AglTestContext context = new AglTestContext(new AglTestStrategySetGetTransform());
+		endUnit(context, false, 400);
+		AffineTransform[] transf_tmp = (AffineTransform[])context.getObjectsStrategy();
+		Assert.assertEquals(transf_tmp[0], transf_tmp[1]);
+	}
+
+	@Test
+	public void testSetGetStroke() throws InterruptedException {
+		AglTestContext context = new AglTestContext(new AglTestStrategySetGetStroke());
+		endUnit(context, false, 80);
+		BasicStroke[] strk_tmp = (BasicStroke[])context.getObjectsStrategy();
+		Assert.assertEquals(strk_tmp[0], strk_tmp[1]);
+	}
+
+	//End test : must be ***THE LAST ONE*** to be called in the list of tests
+	@Test
+	public void testMustBeLastOne() throws InterruptedException {
+
+		frame.setVisible(false);
+		Assert.assertEquals(false, frame.isVisible());
+		frame.remove(glCanvas);
+		frame.dispose();
+		frame=null;
+		glCanvas=null;
+	}
+
+	public void endUnit(AglTestContext _context, boolean updateBothContexts, int delay) throws InterruptedException {
+		sample.setContext(_context);
+		glCanvas.repaint();
+
+		//get Agile Ilage thanks to glReadPixels
+		imgAg2d = sample.getBufferedImage();
+
+		if(updateBothContexts==true){
+			g2dCanvas.setContext(_context);	
+			g2dCanvas.repaint();
+			imgG2d = g2dCanvas.getBufferedImage();
+		}
+		else{
+			g2dCanvas.setContext(null);
+		}
+		try { Thread.sleep(delay);} catch (InterruptedException e) {}
+	}
+
+
+	public void writePreviousDiff(String baseName){
+		File outputfile;
+		//get Agile Front Buffer thanks to glReadPixels
+		imgAg2d = sample.getBufferedImage();
+		//get G2D image drawing directly on a BufferedImage		
 		imgG2d = g2dCanvas.getBufferedImage();
-	}
-	else{
-		g2dCanvas.setContext(null);
-	}
-	try { Thread.sleep(delay);} catch (InterruptedException e) {}
-    }
+		//Create bufferImage which is the difference of the two images
+		image_dif = batikBuildDiffImage(imgAg2d, imgG2d);
+		try{
 
-
-    public void writePreviousDiff(String baseName){
-	File outputfile;
-	//get Agile Ilage thanks to glReadPixels
-	imgAg2d = sample.getBufferedImage();
-	imgG2d = g2dCanvas.getBufferedImage();
-	//Create bufferImage which is the difference of the two images
-	BufferedImage image_dif = batikBuildDiffImage(imgAg2d, imgG2d);
-	try{
-
-		outputfile = new File("diff_"+baseName+".png");
-		ImageIO.write(image_dif, "png", outputfile);
-/*
+			outputfile = new File("diff_"+baseName+".png");
+			ImageIO.write(image_dif, "png", outputfile);
+/*			
 		outputfile = new File("ag2d_"+baseName+".png");
 		ImageIO.write(imgAg2d, "png", outputfile);
 
 		outputfile = new File("g2d_"+baseName+".png");
 		ImageIO.write(imgG2d, "png", outputfile);
-*/
-	}catch (IOException e) { }
-     }
+*/			 
+		}catch (IOException e) { }
+		//Flush buffered images
+		imgAg2d.flush();
+		imgG2d.flush();		
+		image_dif.flush();
+	}
 
-public static BufferedImage batikBuildDiffImage(BufferedImage ref, BufferedImage gen) {
-        BufferedImage diff = new BufferedImage(ref.getWidth(),
-                                               ref.getHeight(),
-                                               BufferedImage.TYPE_INT_RGB);
-        WritableRaster refWR = ref.getRaster();
-        WritableRaster genWR = gen.getRaster();
-        WritableRaster dstWR = diff.getRaster();
+	public static BufferedImage batikBuildDiffImage(BufferedImage ref, BufferedImage gen) {
+		BufferedImage diff = new BufferedImage(ref.getWidth(),
+				ref.getHeight(),
+				BufferedImage.TYPE_INT_RGB);
+		WritableRaster refWR = ref.getRaster();
+		WritableRaster genWR = gen.getRaster();
+		WritableRaster dstWR = diff.getRaster();
 
-        boolean refPre = ref.isAlphaPremultiplied();
-        if (!refPre) {
-            ColorModel     cm = ref.getColorModel();
-            cm = GraphicsUtil.coerceData(refWR, cm, true);
-            ref = new BufferedImage(cm, refWR, true, null);
-        }
-        boolean genPre = gen.isAlphaPremultiplied();
-        if (!genPre) {
-            ColorModel     cm = gen.getColorModel();
-            cm = GraphicsUtil.coerceData(genWR, cm, true);
-            gen = new BufferedImage(cm, genWR, true, null);
-        }
+		boolean refPre = ref.isAlphaPremultiplied();
+		if (!refPre) {
+			ColorModel     cm = ref.getColorModel();
+			cm = GraphicsUtil.coerceData(refWR, cm, true);
+			ref = new BufferedImage(cm, refWR, true, null);
+		}
+		boolean genPre = gen.isAlphaPremultiplied();
+		if (!genPre) {
+			ColorModel     cm = gen.getColorModel();
+			cm = GraphicsUtil.coerceData(genWR, cm, true);
+			gen = new BufferedImage(cm, genWR, true, null);
+		}
 
 
-        int w=ref.getWidth();
-        int h=ref.getHeight();
+		int w=ref.getWidth();
+		int h=ref.getHeight();
 
-        int y, i,val;
-        int [] refPix = null;
-        int [] genPix = null;
-        for (y=0; y<h; y++) {
-            refPix = refWR.getPixels  (0, y, w, 1, refPix);
-            genPix = genWR.getPixels  (0, y, w, 1, genPix);
+		int y, i,val;
+		int [] refPix = null;
+		int [] genPix = null;
+		for (y=0; y<h; y++) {
+			refPix = refWR.getPixels  (0, y, w, 1, refPix);
+			genPix = genWR.getPixels  (0, y, w, 1, genPix);
 
-            for (i=0; i<refPix.length; i++) {
-                val = ((refPix[i]-genPix[i])*10)+128;
-                if ((val & 0xFFFFFF00) != 0)
-                    if ((val & 0x80000000) != 0) val = 0;
-                    else                         val = 255;
-                genPix[i] = val;
-            }
-            dstWR.setPixels(0, y, w, 1, genPix);
-        }
+			for (i=0; i<refPix.length; i++) {
+				val = ((refPix[i]-genPix[i])*10)+128;
+				if ((val & 0xFFFFFF00) != 0)
+					if ((val & 0x80000000) != 0) val = 0;
+					else                         val = 255;
+				genPix[i] = val;
+			}
+			dstWR.setPixels(0, y, w, 1, genPix);
+		}
 
-        if (!genPre) {
-            ColorModel cm = gen.getColorModel();
-            cm = GraphicsUtil.coerceData(genWR, cm, false);
-        }
-        
-        if (!refPre) {
-            ColorModel cm = ref.getColorModel();
-            cm = GraphicsUtil.coerceData(refWR, cm, false);
-        }
+		if (!genPre) {
+			ColorModel cm = gen.getColorModel();
+			cm = GraphicsUtil.coerceData(genWR, cm, false);
+		}
 
-        return diff;
-    }
+		if (!refPre) {
+			ColorModel cm = ref.getColorModel();
+			cm = GraphicsUtil.coerceData(refWR, cm, false);
+		}
+
+		return diff;
+	}
 
 }
