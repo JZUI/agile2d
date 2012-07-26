f45d3632fd778d9114d0111e382b1033be5fbb8e
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2012-07-26 16:09:39 +0000

    3.0.1-SNAPSHOT

--- bench.sh
+++ bench.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Djava.library.path=target/lib -cp target/agile2d-3.0.jar agile2d.benchmark.BenchmarkGUI
+java -Djava.library.path=target/lib -cp target/agile2d-3.0.1-SNAPSHOT.jar agile2d.benchmark.BenchmarkGUI
